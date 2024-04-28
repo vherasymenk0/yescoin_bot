a924d49411f76ba6daf7defd9f7921abf458b085
manqarium <manqarium@gmail.com>
2024-04-28 11:02:59 +0300

    [Feature] add constants

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const SESSION_FOLDER = 'my_sessions'
+export const START_TEXT = `
+░█▀▄▀█ █▀▀ █▀▄▀█ █▀▀ ░█▀▀▀  ▀  ░█▀▀█ █▀▀█ ▀▀█▀▀
+░█░█░█ █▀▀ █ ▀ █ █▀▀ ░█▀▀▀ ▀█▀ ░█▀▀▄ █  █   █
+░█  ░█ ▀▀▀ ▀   ▀ ▀▀▀ ░█    ▀▀▀ ░█▄▄█ ▀▀▀▀   ▀
+
+Select an action:
+
+    1. Create session
+    2. Run clicker
+`
