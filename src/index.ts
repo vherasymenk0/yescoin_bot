b3b535ca17ee3e71ac7f863a847a0d0a8b12841f
vherasymenko <manqarium@gmail.com>
2024-05-02 22:49:11 +0300

    [Feature] setup script runner

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-import { createSession } from './features/createSession'
+import * as input from 'input'
+import { START_TEXT } from './constants'
+import { logger } from './services/logger'
+import { proxyService } from './services/proxyService'
+import { sessionService } from './services/sessionService'
 
-createSession()
+const handleAction = (action: number) => {
+  if (action === 1) sessionService.createSession()
+  else if (action === 2) {
+    const sessionsCount = sessionService.getSessionNames().length
+    const proxiesCount = proxyService.getProxies().length
+
+    logger.info(`Detected ${sessionsCount} sessions | ${proxiesCount} proxies`)
+  }
+}
+
+const start = async (message?: string) => {
+  if (message) logger.warn(message)
+  else logger.info(START_TEXT)
+
+  const actionNumber = Number(await input.text('->: '))
+  const isNumber = !Number.isInteger(actionNumber)
+  const isValid = ![1, 2].includes(actionNumber)
+
+  if (isNumber) start('Action must be number')
+  else if (isValid) start('Action must be 1 or 2')
+  else handleAction(actionNumber)
+}
+
+start()
